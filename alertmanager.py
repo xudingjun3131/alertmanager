cd4520d888078608a7d5f7fad31c17f1f56b9bfd
root <root@Amber-PC.localdomain>
2022-04-19 14:49:57 +0800

    1：修复个别字段为json格式；\n2：增加合并多个alertmanager源的数据

--- alertmanager.py
+++ alertmanager.py
@@ -1,20 +1,20 @@
 # coding: utf-8
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
-#import urllib3
+import urllib3
 from time import sleep
-import time,datetime
+import time, datetime
 import requests
 import MySQLdb
 import MySQLdb.cursors
 from dbutils.pooled_db import PooledDB
-import datetime
 from multiprocessing import Process, Queue, Value, Array
 from threading import Timer
 
-host = ('127.0.0.1', 8888)
+host = ('localhost', 8888)
 
-alertmanager_api_url = 'http://127.0.0.1:9093/api/v2/alerts'
+alertmanager_api_url_list = ['https://alertmanager.xxxx.com/api/v2/alerts', 'https://alertmanager2.xxxx.com/api/v2/alerts']
+
 class DbManager(object):
     def __init__(self, host, port, db_name, user_name, password):
         cmds = ["set names utf8mb4;"]
@@ -44,7 +44,6 @@ def create_db_manager(host='127.0.0.1', port=3306, dbname='alertmanager', userna
     return _db_manager
 
 db = create_db_manager().connection()
-# db = MySQLdb.connect("127.0.0.1", "alertmanager", "alertmanager", "alertmanager", charset='utf8')
 def sqlSelect(sql, db):
         #include:select
         cr = db.cursor()
@@ -54,15 +53,15 @@ def sqlSelect(sql, db):
         return rs
 
 def sqlDMl(sql, db):
-        #include: create
+        #include: create,update,insert,delete,alter
         cr = db.cursor()
         cr.execute(sql)
         db.commit()
         cr.close()
 
-def query_old_alert():
+def query_old_alert(url):
     alert_old_list = []
-    fingerprint_sql = 'select startsAt,fingerprint from alertmanager where statusss="active";'
+    fingerprint_sql = "select startsAt,fingerprint from alertmanager where alertmanager_api_url='%s' and statusss->'$.state'='active' and receivers<>'Watchdog';" %(url)
     rs = sqlSelect(fingerprint_sql, db)
     for n in range(len(rs)):
         startsAt = rs[n]['startsAt']
@@ -72,86 +71,76 @@ def query_old_alert():
     return alert_old_list
 
 class Resquest(BaseHTTPRequestHandler):
-    alert_old_list = query_old_alert()
+    def alert_old_list(url):
+        old_alert = query_old_alert(url)
+        return old_alert
     def do_GET(self):
-        alert_new_list = []
         endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        req = requests.get(url=alertmanager_api_url)
-        data = json.loads(req.text)
-        if len(data) > 0:
-            for i in range(len(data)):
-                metrics_data = data[i]
-                description = metrics_data['annotations'].get('description', '')
-                summary = metrics_data['annotations'].get('summary', '')
-                valuess = metrics_data['annotations'].get('value', '')
-                fingerprint = metrics_data['fingerprint']
-                startsAt = metrics_data['startsAt'].replace('T', ' ').replace('Z', ' ')[0:19]
-                # stime = int(time.mktime(time.strptime(startsAt,"%Y-%m-%d %H:%M:%S")))  #转换为时间戳
-                startsAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(startsAt,"%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
-                endsAt = metrics_data['endsAt'].replace('T', ' ').replace('Z', ' ')[0:19]
-                # endsAt= int(time.mktime(time.strptime(endsAt, "%Y-%m-%d %H:%M:%S")))
-                endsAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(endsAt,"%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
-                receivers = metrics_data['receivers'][0]['name']
-                if len(metrics_data['status']['inhibitedBy']) > 0:
-                    statusss = '抑制告警'
-                elif len(metrics_data['status']['silencedBy']) > 0:
-                    statusss = '静默告警'
-                else:
-                    statusss = metrics_data['status']['state']
-                updatedAt = metrics_data['updatedAt'].replace('T', ' ').replace('Z', ' ')[0:19]
-                # updatedAt = int(time.mktime(time.strptime(updatedAt, "%Y-%m-%d %H:%M:%S")))
-                updatedAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(updatedAt, "%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
-                generatorURL = metrics_data['generatorURL']
-                labels = metrics_data['labels']
-                bb = str(startsAt) + str(fingerprint)
-                alert_new_list.append(bb)
-                if bb not in Resquest.alert_old_list and statusss not in ['静默告警', '抑制告警']:
-                    insert_sql = 'INSERT INTO alertmanager (description,summary,valuess,fingerprint,startsAt,endsAt,receivers,updatedAt,statusss,generatorURL,labels) VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s");' %(description,summary,valuess,fingerprint,startsAt,endsAt,receivers,updatedAt,statusss,generatorURL,labels)
-                    # print(insert_sql)
-                    sqlDMl(insert_sql, db)
-
-        ##### 修正恢复时间
-        # print('alert_new_list:' + str(alert_new_list))
-        resolved_list = list(set(Resquest.alert_old_list).difference(set(alert_new_list)))
-        if len(resolved_list) > 0:
-            for bb in resolved_list:
-                startsAt = bb[0:19]
-                fingerprint = bb[19:36]
-                #print(startsAt)
-                #print(fingerprint)
-                update_sql = 'update alertmanager set endsAt="%s",statusss="resolved" where startsAt="%s" and fingerprint="%s";' % (endtime, startsAt, fingerprint)
-                sqlDMl(update_sql, db)
-        Resquest.alert_old_list = alert_new_list
-
-
-
-
-
-        rsp_code = req.status_code
-        self.send_response(rsp_code)
-        self.send_header('Content-type', 'application/json')  #处理头部为json格式
-        # self.send_header('Connection', 'keep-alive')
-        # self.send_header('Content-type', 'text/html; charset=utf-8')  #处理数据为text格式
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode()) #返回json格式数据
-        # self.wfile.write(str(data).encode()) #返回text格式数据
+        for alertmanager_api_url in alertmanager_api_url_list:
+            alert_new_list = []
+            req = requests.get(url=alertmanager_api_url)
+            data = json.loads(req.text)
+            if len(data) > 0:
+                for i in range(len(data)):
+                    metrics_data = data[i]
+                    description = metrics_data['annotations'].get('description', '')
+                    summary = metrics_data['annotations'].get('summary', '')
+                    valuess = metrics_data['annotations'].get('value', '')
+                    fingerprint = metrics_data['fingerprint']
+                    startsAt = metrics_data['startsAt'].replace('T', ' ').replace('Z', ' ')[0:19]
+                    startsAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(startsAt,"%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                    endsAt = metrics_data['endsAt'].replace('T', ' ').replace('Z', ' ')[0:19]
+                    endsAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(endsAt,"%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                    receivers = metrics_data['receivers'][0]['name']
+                    statusss = json.dumps(metrics_data['status'])
+                    updatedAt = metrics_data['updatedAt'].replace('T', ' ').replace('Z', ' ')[0:19]
+                    updatedAt = (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(updatedAt, "%Y-%m-%d %H:%M:%S")))) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                    generatorURL = metrics_data['generatorURL']
+                    labels = json.dumps(metrics_data['labels'])
+                    bb = str(startsAt) + str(fingerprint)
+                    alert_new_list.append(bb)
+                    if receivers == "Watchdog":
+                        continue
+                    else:
+                        if bb not in Resquest.alert_old_list(url=alertmanager_api_url):
+                            sql_arg = (alertmanager_api_url, description, summary, valuess, fingerprint, startsAt, endsAt, receivers, updatedAt, statusss, generatorURL, labels)
+                            insert_sql = "INSERT INTO alertmanager (alertmanager_api_url, description, summary, valuess, fingerprint, startsAt, endsAt, receivers, updatedAt, statusss, generatorURL, labels) VALUES " + str(sql_arg) +";"
+                            sqlDMl(insert_sql, db)
+            ##### 修正恢复时间
+            resolved_list = list(set(Resquest.alert_old_list(url=alertmanager_api_url)).difference(set(alert_new_list)))
+            if len(resolved_list) > 0:
+                for bb in resolved_list:
+                    startsAt = bb[0:19]
+                    fingerprint = bb[19:36]
+                    statusss = json.dumps({"state": "resolved", "silencedBy": [], "inhibitedBy": []})
+                    update_sql = "update alertmanager set endsAt='%s',statusss='%s' where startsAt='%s' and fingerprint='%s';" % (endtime, statusss, startsAt, fingerprint)
+                    sqlDMl(update_sql, db)
+            #Resquest.alert_old_list = alert_new_list
+            rsp_code = req.status_code
+            self.send_response(rsp_code)
+            self.send_header('Content-type', 'application/json')  #处理头部为json格式
+            # self.send_header('Connection', 'keep-alive')
+            # self.send_header('Content-type', 'text/html; charset=utf-8')  #处理数据为text格式
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode()) #返回json格式数据
+            # self.wfile.write(str(data).encode()) #返回text格式数据
 # 定义一个新的长连接，减少time_wait
-#def long_conn_localhost():
-#    client = requests.Session()
-#    headers = {'Connection': 'keep-alive'}
-#    while True:
-#        url = client.get(url='http://127.0.0.1:8888', headers=headers)
-        #print(url.status_code)
+def long_conn_localhost():
+    client = requests.Session()
+    headers = {'Connection': 'keep-alive'}
+    while True:
+        url = client.get(url='http://127.0.0.1:8888', headers=headers)
+        return url.status_code
 
 # 改用urllib3的长连接
-#def long_conn_urllib3_localhost():
-#    http = urllib3.PoolManager(num_pools=100, headers={'Connection':'keep-alive'}, maxsize=100, block=True)
-#    while True:
-#      http.request('GET', 'http://127.0.0.1:8888')
-#      sleep(1)
+def long_conn_urllib3_localhost():
+    http = urllib3.PoolManager(num_pools=100, headers={'Connection':'keep-alive'}, maxsize=100, block=True)
+    while True:
+      http.request('GET', 'http://127.0.0.1:8888')
+      sleep(1)
 
 def curl_localhost():
-    rr = requests.get(url='http://127.0.0.1:8888/alert')
+    rr = requests.get(url='http://127.0.0.1:8888')
     return rr.status_code
 
 
@@ -178,5 +167,4 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
 #    p2.terminate()
-
 
--- alertmanager.sql
+++ alertmanager.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE IF NOT EXISTS `alertmanager` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 DROP TABLE IF EXISTS `alertmanager`;
 CREATE TABLE `alertmanager` (
-  `description` varchar(500) DEFAULT NULL,
-  `summary` varchar(500) DEFAULT NULL,
-  `valuess` varchar(255) DEFAULT NULL,
-  `fingerprint` varchar(255) DEFAULT NULL,
-  `startsAt` varchar(255) DEFAULT NULL,
-  `endsAt` varchar(255) DEFAULT NULL,
-  `updatedAt` varchar(255) DEFAULT NULL,
-  `statusss` varchar(255) DEFAULT NULL,
-  `generatorURL` varchar(500) DEFAULT NULL,
-  `labels` varchar(500) DEFAULT NULL,
-  `receivers` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `unit_fingerprint_and_stime` (`startsAt`,`fingerprint`) USING BTREE
+	  `alertmanager_api_url` varchar(500) DEFAULT NULL,
+	  `description` varchar(500) DEFAULT NULL,
+	  `summary` varchar(500) DEFAULT NULL,
+	  `valuess` varchar(255) DEFAULT NULL,
+	  `fingerprint` varchar(255) DEFAULT NULL,
+	  `startsAt` varchar(255) DEFAULT NULL,
+	  `endsAt` varchar(255) DEFAULT NULL,
+	  `updatedAt` varchar(255) DEFAULT NULL,
+	  `statusss` JSON,
+	  `generatorURL` varchar(500) DEFAULT NULL,
+	  `labels` JSON,
+	  `receivers` varchar(255) DEFAULT NULL,
+	  UNIQUE KEY `unit_fingerprint_and_stime` (`startsAt`,`fingerprint`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 GRANT ALL ON `alertmanager`.* TO `alertmanager`@"%"  IDENTIFIED BY 'alertmanager';
 
